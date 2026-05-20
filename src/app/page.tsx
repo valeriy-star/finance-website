ae8d482a39c74a3bda551172cc3b4863a78ee9f0
valeriy-star <valeriy@kosovan.finance>
2026-05-20 16:22:07 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import { EvolutionPreview } from '@/components/EvolutionPreview';
 import { ExecutiveSummaryPreview } from '@/components/ExecutiveSummaryPreview';
 import { FeaturedArticles } from '@/components/FeaturedArticles';
 import { ContactCTA } from '@/components/ContactCTA';
+import { PersonSchema } from '@/components/StructuredData';
 
 export default function HomePage() {
   return (
     <>
+      <PersonSchema />
       <HeroHome />
       <ValueProposition />
       <ExpertisePreview />
